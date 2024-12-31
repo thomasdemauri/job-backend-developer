2e89b56b421515edca4cfc6f011f0556c8c636fe
Thomas Mauro <thom_mauro@outlook.com>
2024-12-31 14:11:09 -0300

    Seeding initial products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,11 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->unique()->word(),
+            'price' => $this->faker->randomFloat(2, 15, 340.40),
+            'description' => $this->faker->text(),
+            'category' => $this->faker->randomElement(['eletronics', 'home', 'health', 'sports']), // Between elements
+            'image_url' => $this->faker->imageUrl()
         ];
     }
 }
