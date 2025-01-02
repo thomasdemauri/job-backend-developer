c17d659d83b37041322854eeb834d89d72d1dbc3
Thomas Mauro <thom_mauro@outlook.com>
2025-01-01 22:23:06 -0300

    product resource added

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'price' => $this->price,
+            'description' => $this->description,
+            'category' => $this->category,
+            'image' => $this->when($request->query('includeImage'), $this->image_url)
+        ];
+    }
+}
